5d50835cd70c1fba0ca07c0c8ead502d1c780b3d
SenenLara35 <senenlara35@gmail.com>
2025-12-03 21:53:18 +0100

    Arreglos para evitar error 405

--- src/slack_service/main.py
+++ src/slack_service/main.py
@@ -1,33 +1,22 @@
 from fastapi import FastAPI, HTTPException
-from .schemas import SlackMessageRequest
 from fastapi.staticfiles import StaticFiles
-from .slack_client import send_message_to_slack
 import os
 
-app = FastAPI(
-    title="Servicio FastAPI Slack",
-    version="1.0.0"
-)
+from .schemas import SlackMessageRequest
+from .slack_client import send_message_to_slack
 
-# --- BLOQUE NUEVO PARA LA WEB ---
-# Calculamos la ruta absoluta a la carpeta static para que no falle en Linux
-static_path = os.path.join(os.path.dirname(__file__), "static")
+app = FastAPI(title="Servicio FastAPI Slack", version="1.0.0")
 
-# Montamos la web en la raíz "/"
-app.mount("/", StaticFiles(directory=static_path, html=True), name="static")
-# --------------------------------
+# --- 1. PRIMERO DEFINIMOS LA API (Para que tenga prioridad) ---
 
 @app.get("/health")
 def health_check():
-    """Endpoint para monitoreo."""
     return {"status": "ok"}
 
 @app.post("/send-slack-message")
 def send_slack_message(payload: SlackMessageRequest):
-    # Se pasa la información validada al cliente de Slack
     result = send_message_to_slack(text=payload.text, channel=payload.channel)
 
-    # Si Slack responde "ok": False, se lanza un error HTTP 500
     if not result["ok"]:
         raise HTTPException(status_code=500, detail=result["error"])
 
@@ -36,3 +25,8 @@ def send_slack_message(payload: SlackMessageRequest):
         "channel": result["channel"],
         "ts": result["ts"]
     }
+
+# --- 2. LUEGO MONTAMOS LA WEB (Como último recurso) ---
+# Si la petición no coincide con la API de arriba, servirá el HTML.
+static_path = os.path.join(os.path.dirname(__file__), "static")
+app.mount("/", StaticFiles(directory=static_path, html=True), name="static")
